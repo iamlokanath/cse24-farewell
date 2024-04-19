1578b8c6a29b00d7ed448f0b2e07c0d5b982426d
Lkanath Panda <lokanthpanda128@gmai.com>
2024-04-19 15:09:35 +0530

    First commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './css/Home.css';
+const Home = () => {
+  return (
+    <>
+    <div className="section">
+        <div className="content">
+            <div className="info">
+                <p>
+
+                   
+                    Dear Seniors,
+                    <br />
+                    <br />
+                    As the pages turn and the time comes to bid adieu, we, your juniors from the Department of Computer
+                    Science and Engineering, are honored to extend this invitation to an unforgettable <span className="movie-night">farewell</span>,
+                    gathering in your honor. Join us on April 21, 2024, at the Seminar Hall, as we come together to
+                    celebrate the profound impact you've had on our journey.
+                    <br />
+                    <br />
+                    Date: April 21, 2024
+                    <br />
+                    Venue: Seminar Hall
+                    <br />
+                    Time: 2:30 PM
+                </p>
+                <button className="btn"> <Link to = "/testimonials">Click Here</Link></button>
+            </div>
+        </div>
+
+        <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+
+
+    </div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Thankyou.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./css/Thankyou.css";
+const Thankyou = () => {
+  return (
+    <>
+      <div className="card-body">
+        <div className="card">
+          Dear Senior, as our journey together comes to a close, we, your junior
+          family from the Department of Computer Science and Engineering, want
+          to express our heartfelt gratitude to you. Your guidance, kindness,
+          and unwavering support have shaped us in profound ways, inspiring us
+          to reach for greatness. The memories we've shared and the lessons
+          we've learned will forever be cherished. Thank you for being the
+          incredible seniors that you are, and know that your legacy will
+          continue to inspire us. With sincere appreciation 3rd year CSE,
+          Department of Computer Science and Engineering.
+          <br />
+          THANK YOU
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Thankyou;
