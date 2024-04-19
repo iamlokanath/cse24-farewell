9f159dc9fa9b7e509fb3b1023f6eb60f231049e0
Lkanath Panda <lokanthpanda128@gmai.com>
2024-04-19 17:28:23 +0530

    error page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import Testimonials from './components/Testimonials';
 import Thankyou from './components/Thankyou';
+import PagenotFound from './components/PagenotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/testimonials' element={<Testimonials/>}/>
         <Route path='/thankyou' element={<Thankyou/>}/>
+        <Route path='*' element={<PagenotFound/>}/>
       </Routes>
       </BrowserRouter>
     </div>
